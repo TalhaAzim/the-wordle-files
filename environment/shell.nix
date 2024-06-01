ee0a976a264a2ca0600b0cf816148d277cbed524
Talha B <talha.azim@gmail.com>
2024-05-31 21:53:29 -0400

    updated environment

--- environment/shell.nix
+++ environment/shell.nix
@@ -1,16 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.python12
-    pkgs.python12Packages.numpy
-    pkgs.python12Packages.pandas
-    pkgs.python12Packages.matplotlib
-    pkgs.python12Packages.seaborn
-    pkgs.python12Packages.jupyterlab
+# shell.nix
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      # select Python packages here
+      python-pkgs.numpy
+      python-pkgs.pandas
+      python-pkgs.matplotlib
+      python-pkgs.seaborn
+      python-pkgs.jupyterlab
+    ]))
   ];
-
-  shellHook = ''
-    export PYTHONPATH=$PYTHONPATH:$(pwd)/src
-  '';
 }
